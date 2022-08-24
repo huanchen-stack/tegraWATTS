55728cd1fa23e206002bbdd5154c3b9a26d4c5d6
huanchen-stack <huanchen@umich.edu>
2022-08-24 00:53:58 -0400

    spinning clock

--- spinningclock.py
+++ spinningclock.py
@@ -1,7 +1,30 @@
 import time
 from datetime import datetime
+import keyboard
 
-with open("tegralog.txt", 'a') as f:
-    for i in range(10):
-        time.sleep(1)
-        f.write(f"{datetime.now()}\n")
+import sys
+import subprocess as sp
+
+LOGFILE = 'tegralog.txt'
+METHOD = 'w'
+
+
+def read_tegrastats(interval=1):
+    pts = sp.Popen(['tegrastats', '--interval', str(interval)], stdout=sp.PIPE)
+    with open(LOGFILE, METHOD) as f:
+        try:
+            while True:
+                print(datetime.now())
+                if pts.poll() is not None:
+                    continue
+                out = pts.stdout
+                if out is not None:
+                    line = out.readline()
+                    tegrastats_data = line.decode("utf-8")
+                    f.write(f"{datetime.now().replace(microsecond=0)} {tegrastats_data}")
+        finally:
+            pts.kill()
+
+
+read_tegrastats()
+
---
+++ timespin.py
@@ -0,0 +1,5 @@
+import time
+from datetime import datetime
+
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
